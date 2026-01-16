580d5ebbc26f6ef7316bc87dd42fedef2ce49812
rifsxd <rifat.44.azad.rifs@gmail.com>
2026-01-17 01:06:40 +0600

    kernel: simplify rehook and hook syscall individually

--- kernel/patch/common/rehook.c
+++ kernel/patch/common/rehook.c
@@ -9,26 +9,15 @@
 #include <symbol.h>
 #include <linux/printk.h>
 #include <kputils.h>
-
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
-
-extern void *sys_call_table;
-extern void *compat_sys_call_table;
-extern int has_config_compat;
+#include <hook.h>
+#include <stddef.h>
 
 static int rehook_enabled = 0;
+\
+static int getpriority_nr = __NR_getpriority;
+static int getpriority_nr_arg = 2;
 
-static int nr_arg = 2;
-
-static const int rehook_native_syscalls[] = {
-    __NR_getpriority
-};
-
-// static const int rehook_compat_syscalls[] = {
-//     96
-// };
-
-static void rehook_before(hook_fargs2_t *args, void *udata)
+static void getpriority_before(hook_fargs2_t *args, void *udata)
 {
     if (current_uid() != 0u) // only allow root 
         return;
@@ -45,111 +34,42 @@ static inline bool syscall_is_valid(int nr)
     return true;
 }
 
-// static inline bool compat_syscall_is_valid(int nr)
-// {
-//     void **table;
-    
-//     table = (void **)compat_sys_call_table;
-//     if (!table || table[nr] == NULL)
-//         return false;
-    
-//     return true;
-// }
+static void hook_getpriority_syscall(void)
+{   
+    logki("hook_getpriority_syscall start\n");
 
-static void hook_native_syscalls(void)
-{
-    logki("hook_native_syscalls start\n");
-
-    for (size_t i = 0; i < ARRAY_SIZE(rehook_native_syscalls); i++) {
-        int nr = rehook_native_syscalls[i];
-        
-        // skip invalid/unimplemented syscalls
-        if (!syscall_is_valid(nr)) {
-            logkfd("skipping invalid native syscall %d\n", nr);
-            continue;
-        }
-        
-        hook_syscalln(nr, nr_arg, rehook_before, 0, 0);
-        logkfd("hooked native syscall %d\n", nr);
+    if (!syscall_is_valid(getpriority_nr)) {
+        logkfd("skipping invalid native syscall %d\n", getpriority_nr);
+        return;
     }
-
-    logki("hook_native_syscalls done\n");
+    
+    hook_syscalln(getpriority_nr, getpriority_nr_arg, getpriority_before, 0, 0);
+    logkfd("hooked native syscall %d\n", getpriority_nr);
+    
+    logki("hook_getpriority_syscall done\n");
 }
 
-static void unhook_native_syscalls(void)
-{
-    logki("unhook_native_syscalls start\n");
+static void unhook_getpriority_syscall(void)
+{    
+    logki("unhook_getpriority_syscall start\n");
 
-    for (size_t i = 0; i < ARRAY_SIZE(rehook_native_syscalls); i++) {
-        int nr = rehook_native_syscalls[i];
-        
-        // skip invalid/unimplemented syscalls
-        if (!syscall_is_valid(nr)) {
-            logkfd("skipping invalid native syscall %d\n", nr);
-            continue;
-        }
-        
-        unhook_syscalln(nr, rehook_before, 0);
-        logkfd("unhooked native syscall %d\n", nr);
+    if (!syscall_is_valid(getpriority_nr)) {
+        logkfd("skipping invalid native syscall %d\n", getpriority_nr);
+        return;
     }
 
-    logki("unhook_native_syscalls done\n");
+    unhook_syscalln(getpriority_nr, getpriority_before, 0);
+    logkfd("unhooked native syscall %d\n", getpriority_nr);
+
+    logki("unhook_getpriority_syscall done\n");
 }
-
-/* ---------------- Compat ---------------- */
-// static void hook_compat_syscalls(void)
-// {
-//     if (!has_config_compat || !compat_sys_call_table)
-//         return;
-
-//     logki("hook_compat_syscalls start\n");
-
-//     for (size_t i = 0; i < ARRAY_SIZE(rehook_compat_syscalls); i++) {
-//         int nr = rehook_compat_syscalls[i];
-        
-//         // skip invalid/unimplemented syscalls
-//         if (!compat_syscall_is_valid(nr)) {
-//             logkfd("skipping invalid compat syscall %d\n", nr);
-//             continue;
-//         }
-        
-//         hook_compat_syscalln(nr, 0, rehook_before, 0, 0);
-//         logkfd("hooked compat syscall %d\n", nr);
-//     }
-
-//     logki("hook_compat_syscalls done\n");
-// }
-
-// static void unhook_compat_syscalls(void)
-// {
-//     if (!has_config_compat || !compat_sys_call_table)
-//         return;
-
-//     logki("unhook_compat_syscalls start\n");
-
-//     for (size_t i = 0; i < ARRAY_SIZE(rehook_compat_syscalls); i++) {
-//         int nr = rehook_compat_syscalls[i];
-        
-//         // skip invalid/unimplemented syscalls
-//         if (!compat_syscall_is_valid(nr)) {
-//             logkfd("skipping invalid compat syscall %d\n", nr);
-//             continue;
-//         }
-        
-//         unhook_compat_syscalln(nr, rehook_before, 0);
-//         logkfd("unhooked compat syscall %d\n", nr);
-//     }
-
-//     logki("unhook_compat_syscalls done\n");
-// }
 
 /* ---------------- Init / Exit ---------------- */
 int rehook_init(void)
 {
     logki("rehook_init start\n");
 
-    hook_native_syscalls();
-    // hook_compat_syscalls();
+    hook_getpriority_syscall();
 
     rehook_enabled = 1;
 
@@ -162,8 +82,7 @@ int rehook_exit(void)
 {
     logki("rehook_exit start\n");
 
-    unhook_native_syscalls();
-    // unhook_compat_syscalls();
+    unhook_getpriority_syscall();
 
     rehook_enabled = 0;
 
